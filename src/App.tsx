d61b100b78d9dbb9fda50f3eb0791e73f70af079
tienviet04 <abvutienviet@gmail.com>
2024-08-29 13:35:18 +0700

    done sign in, sign up, 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./app.scss";
 import UserLayout from "./layouts/UserLayout";
+import SignIn from "./components/signIn-signUp/signIn";
+import SignUp from "./components/signIn-signUp/signUp";
+import NotFoundPage from "./components/404/404";
 
 function App() {
     const router = createBrowserRouter([
@@ -10,6 +13,18 @@ function App() {
                 {
                     path: "/",
                     element: <div>p</div>,
+                },
+                {
+                    path: "/login",
+                    element: <SignIn />,
+                },
+                {
+                    path: "/signup",
+                    element: <SignUp />,
+                },
+                {
+                    path: "/404",
+                    element: <NotFoundPage />,
                 },
             ],
         },
