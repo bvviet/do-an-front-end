65b1a395a7616fe4af1de1f3530420fb562bf9d3
bvviet <lanviethoang2004@thoang2004gmail.g2004gmailcom>
2024-09-03 20:49:30 +0700

    fig devloy

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import BlogItem from './blogItem'
+import { useState } from "react";
+import BlogItem from "./blogItem";
 
 export default function BlogCompoment() {
     const [visibleCount, setVisibleCount] = useState(6); // Số lượng item hiển thị ban đầu
 
     // Mẫu item mặc định
     const defaultItem = {
-        title: 'Kỉ niệm ngày 2/9',
-        img: 'https://hadong.hanoi.gov.vn/SiteAssets/Lists/NewsList/AllItems/treo%20co%20to%20quoc%20QK%202921.jpg',
-        date: 'Sep 9, 2024',
-        author: 'TienViet',
+        title: "Kỉ niệm ngày 2/9",
+        img: "https://hadong.hanoi.gov.vn/SiteAssets/Lists/NewsList/AllItems/treo%20co%20to%20quoc%20QK%202921.jpg",
+        date: "Sep 9, 2024",
+        author: "TienViet",
     };
 
     // Mảng chỉ số
@@ -35,21 +35,25 @@ export default function BlogCompoment() {
 
     return (
         <div className="">
-            <h1 className="font-slab font-bold text-[42px] max-sm:text-[28px] text-[#131717] py-32  hover:underline" style={{ lineHeight: "123.81%" }}>Latest from the team</h1>
+            <h1
+                className="font-slab font-bold text-[42px] max-sm:text-[28px] text-[#131717] py-32  hover:underline"
+                style={{ lineHeight: "123.81%" }}
+            >
+                Latest from the team
+            </h1>
             <div className="grid grid-cols-3 gap-12 max-sm:gap-6 max-sm:grid-cols-2 mx-auto">
                 {arr.map((item) => (
-                    <BlogItem
-                        key={item.id}
-                        title={item.title}
-                        img={item.img}
-                        date={item.date}
-                        author={item.author}
-                    />
+                    <BlogItem key={item.id} title={item.title} img={item.img} date={item.date} author={item.author} />
                 ))}
             </div>
             <div className="w-full flex justify-center mt-[80px] max-lg:mt-[52px]">
-                <button type="button" onClick={handleShowMore} className="flex justify-center text-white bg-[#005D63] hover:bg-[#274b4f] rounded text-[18px] max-md:text-[14px] font-semibold px-[32px] py-4 mb-2" style={{ lineHeight: "166.67%" }}>
-                    {showHide ? 'Load more' : 'Hide all'}
+                <button
+                    type="button"
+                    onClick={handleShowMore}
+                    className="flex justify-center text-white bg-[#005D63] hover:bg-[#274b4f] rounded text-[18px] max-md:text-[14px] font-semibold px-[32px] py-4 mb-2"
+                    style={{ lineHeight: "166.67%" }}
+                >
+                    {showHide ? "Load more" : "Hide all"}
                 </button>
             </div>
         </div>
