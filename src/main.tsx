f347170e55809a9be59f98606e1e997a6f6ffcc4
bvviet <lanviethoang2004@thoang2004gmail.g2004gmailcom>
2024-09-06 00:40:54 +0700

    update ui header-footer

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./scss/base/index.scss";
 import "./index.css";
 import ModalProvider from "./contexts/ModelPopUp/ModelProvider.tsx";
+import { OverlayProvider } from "./contexts/Overlay.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ModalProvider>
-            <App />
+            <OverlayProvider>
+                <App />
+            </OverlayProvider>
         </ModalProvider>
     </StrictMode>
 );
