bfd8ccf5936280a34a37d1d4c479a45ad3e6a916
tienviet04 <abvutienviet@gmail.com>
2024-09-12 14:39:23 +0700

    done check out

---
+++ src/pages/user/CheckOut/CheckOut.tsx
@@ -0,0 +1,11 @@
+import CheckOutComponent from "../../../components/CheckOutComponents/CheckOutComponents";
+
+export default function CheckOut() {
+  return (
+    <>
+      <div className="container my-24">
+        <CheckOutComponent />
+      </div>
+    </>
+  );
+}
