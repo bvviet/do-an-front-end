af1268bcb22393f1f27e461d32ef312f193614d2
bvviet <lanviethoang2004@thoang2004gmail.g2004gmailcom>
2024-09-18 15:05:57 +0700

    build base admin

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,6 +1,17 @@
+import NavbarAdmin from "@/components/admin/Navbar";
+import { Outlet } from "react-router-dom";
 const AdminLayout = () => {
   return (
-    <div>AdminLayout</div>
-  )
-}
-export default AdminLayout
+    <div className="bg-[#F5F5F5]">
+      <div className="container flex">
+        <div className="w-1/4">
+          <NavbarAdmin />
+        </div>
+        <div className="content w-3/4 bg-white p-7">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AdminLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: "class",
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        container: {
-            center: true,
-            padding: {
-                DEFAULT: "24px",
-                sm: "24px",
-                lg: "4rem",
-                xl: "5rem",
-            },
-            screens: {
-                sm: "640px",
-                md: "768px",
-                lg: "1024px",
-                xl: "1270px",
-            },
-        },
-        extend: {
-            fontFamily: {
-                slab: ['"Roboto Slab"', "serif"],
-                manrope: ['"Manrope", sans-serif'],
-            },
-            colors: {
-                "custom-white": "#ffffff",
-                "custom-dark": "#1e293b", // màu tối
-                "custom-indigo": "#4f46e5", // màu indigo
-                "custom-gray": "#000", // màu xám sáng
-                "custom-dark-gray": "#374151", // màu xám tối
-            },
-            boxShadow: {
-                "custom-shadow": "0 0 5px rgba(0, 0, 0, 0.1), 0 0 1px rgba(0, 0, 0, 0.1)",
-            },
-            animation: {
-                spin: "spin 1s linear infinite", // Define the custom spin animation
-            },
-            keyframes: {
-                spin: {
-                    "0%": { transform: "rotate(0deg)" },
-                    "100%": { transform: "rotate(360deg)" },
-                },
-            },
-        },
+  darkMode: "class",
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "24px",
+        sm: "24px",
+        lg: "4rem",
+        xl: "5rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1270px",
+      },
     },
-    plugins: [],
+    extend: {
+      fontFamily: {
+        slab: ['"Roboto Slab"', "serif"],
+        manrope: ['"Manrope", sans-serif'],
+      },
+      colors: {
+        main: "#005d63",
+        "custom-white": "#ffffff",
+        "custom-dark": "#1e293b", // màu tối
+        "custom-indigo": "#4f46e5", // màu indigo
+        "custom-gray": "#000", // màu xám sáng
+        "custom-dark-gray": "#374151", // màu xám tối
+      },
+      boxShadow: {
+        "custom-shadow":
+          "0 0 5px rgba(0, 0, 0, 0.1), 0 0 1px rgba(0, 0, 0, 0.1)",
+      },
+      animation: {
+        spin: "spin 1s linear infinite", // Define the custom spin animation
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
 };
